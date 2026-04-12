604b84ee26058796f26c3146b7944109a902682b
Barhoumi819 <mohamedyoussefbarhoumi6@gmail.com>
2026-04-12 20:32:07 +0300

    I added some changes in hello.py

--- hello.py
+++ hello.py
@@ -4,3 +4,10 @@
 import pandas as pd
 import numpy as np
 print("Hello world")
+
+# hello guys 
+# hello guys 
+# hello guys 
+# hello guys 
+# hello guys 
+# hello guys 
--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 import os
+
+# hello guys 
+# the blu bug in ao1 is a true bug and i think it can be exploited
+# dsaaaaaaaa
